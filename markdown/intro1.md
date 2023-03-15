c8a65636a4b02ffaaf55df73ec57169f6cfc8d78
hmarch <hmarch@cs.washington.edu>
2023-03-14 23:39:24 -0700

    intro/problem statement

---
+++ markdown/intro1.md
@@ -0,0 +1,5 @@
+## Nucleation Site Analysis for HIV Viral Load monitoring
+
+*Viral Load - a measure of how much virus is in someone's body, usually an indicator for the severity of an infection.*
+
+As of 2021, there were approximately 38.4 million people living with HIV. People living with HIV require routine viral load monitoring in order to measure treatment efficacy and adherence. Polymerase chain reaction (PCR) is the gold standard for measuring viral load (think at-clinic COVID testing), but is not accessible to many clinics and patients around the world due to its high cost and long wait times. As a result, PCR is limited to centralized testing facilities far from the point-of-care (POC), leading to delayed results or loss of follow-up. The majority of people living with HIV live in low- and middle-income countries where PCR testing is largely inaccessible, so there is a need to develop a robust POC viral load test.
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,27 @@
 import streamlit as st
+from pathlib import Path
 
-st.title("Hello CSE455!")
+def read_markdown_file(markdown_file):
+  return Path(markdown_file).read_text()
+
+def caption(text):
+  return f'<em style="text-align: center">{text}</em>'
+
+st.markdown(read_markdown_file("markdown/intro1.md"))
+st.image("figs/hivstats.png")
+st.markdown(caption("Data from WHO"), unsafe_allow_html=True)
+st.markdown(read_markdown_file("markdown/intro2.md"))
+st.markdown(caption("Top: number of copies of HIV DNA. A higher copy number means higher viral load."), unsafe_allow_html=True)
+st.image("figs/membranespots.png")
+st.markdown(caption("RPA Performed on fiber membrane. Quantification becomes difficult at higher copy numbers."), unsafe_allow_html=True)
+
+
+st.header("Objective")
+st.markdown(read_markdown_file("markdown/objective1.md"))
+st.image("figs/glass_slide_data_1e5cps.png")
+st.markdown(caption("RPA Performed on glass slides with 10,000cps HIV DNA. Left: low viscosity, Right: high viscosity. " + 
+                    "This demonstrates that it may be possible to quantify at higher copy numbers."), unsafe_allow_html=True)
+
+
+st.header("Methods")
+st.markdown(read_markdown_file("markdown/data_collection1.md"), unsafe_allow_html=True)
